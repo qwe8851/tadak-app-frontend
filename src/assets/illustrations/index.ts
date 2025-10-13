6c8adb2a8f2fedf40480e9d45f75d393e8d6b519
qwe8851 <dgh07027@naver.com>
2025-10-14 01:07:16 +0900

    feat: assets 파일 구조 생성
    - 이미지 추후 추가 예정

---
+++ src/assets/illustrations/index.ts
@@ -0,0 +1,6 @@
+// 일러스트, 배경용 이미지
+
+export const Illustrations = {
+  onboarding: require("./onboarding.png"),
+  emptyState: require("./empty-state.png"),
+};
--- src/assets/images/Index.ts
+++
@@ -1,5 +0,0 @@
-export const Images = {
-  logo: require("./logo.png"),
-  splash: require("./splash-icon.png"),
-  profilePlaceholder: require("./profile-placeholder.png"),
-};
